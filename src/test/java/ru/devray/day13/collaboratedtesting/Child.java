c558ce35e9b7003168d56b61cb854ec9967ac209
Aleksandr Kuzmichev <d5f@ya.ru>
2021-08-27 23:01:14 +0300

    Created first market cap test.

---
+++ src/test/java/ru/devray/day13/collaboratedtesting/Child.java
@@ -0,0 +1,13 @@
+package ru.devray.day13.collaboratedtesting;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Child {
+
+    {
+        System.out.println("inside non static block");
+    }
+
+
+}
