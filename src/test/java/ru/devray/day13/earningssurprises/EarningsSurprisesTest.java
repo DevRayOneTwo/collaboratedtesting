b66bface74e40a1905c31b731009a468d9ac0290
Nikolay Rybakov <mr.Nikolay.Rybakov@yandex.ru>
2021-08-29 02:34:43 +0300

    Create dto EarningsSurprises and finished autotests

--- src/test/java/ru/devray/day13/earningssurprises/EarningsSurprisesTest.java
+++ src/test/java/ru/devray/day13/earningssurprises/EarningsSurprisesTest.java
@@ -2,7 +2,6 @@ package ru.devray.day13.earningssurprises;
 
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
-import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.hamcrest.Matchers;
@@ -10,10 +9,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import ru.devray.day13.dto.EarningsSurprises;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.IntStream;
 
 import static io.restassured.RestAssured.given;
 
@@ -22,9 +21,8 @@ public class EarningsSurprisesTest {
     static RequestSpecification spec;
 
     final static String TOKEN = "c224fcc7126c0f35b156d6cb915a12a6";
-//    final static String URI = "https://financialmodelingprep.com/";
-//    final static String PATH = "api/v3/earnings-surprises";
 
+    //настройка спецификации
     @BeforeEach
     void setUp() {
         RequestSpecBuilder builder = new RequestSpecBuilder();
@@ -34,8 +32,10 @@ public class EarningsSurprisesTest {
         spec = builder.build();
     }
 
+    //проверка статус кода и наличие в header content-type
+    //с FMP AAPL
     @Test
-    void testStatusCodeAndContentType() {
+    void testStatusCodeAndContentTypeAAPL() {
         given()
                 .spec(spec)
                 .queryParam("apikey", TOKEN)
@@ -43,39 +43,90 @@ public class EarningsSurprisesTest {
                 .when()
                 .get("/AAPL")
                 .then()
+                .log().all()
+                .assertThat()
                 .statusCode(200)
                 .header("Content-Type", "application/json;charset=UTF-8");
     }
 
-    @ParameterizedTest
+    //проверка статус кода и наличие в header content-type
+    //с FMP FB
+    @Test
+    void testStatusCodeAndContentTypeFB() {
+        given()
+                .spec(spec)
+                .queryParam("apikey", TOKEN)
+                .log().all()
+                .when()
+                .get("/FB")
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(200)
+                .header("Content-Type", "application/json;charset=UTF-8");
+    }
+
+    //негативная проверка метода get
+    //не передаем в метод FMP. Ждем 404
+    @Test
+    void testNegativeMethodGet() {
+        given().spec(spec)
+                .queryParam("apikey", TOKEN)
+                .when()
+                .get()
+                .then()
+                .log().all()
+                .statusCode(404)
+                .body("error", Matchers.equalTo("Not Found"));
+    }
+
+    //параметризованный тест на проверку в JSON поля date и symbol
+    //подаем один RegEx и разные FMP
+    @ParameterizedTest(name = "#{index} - test {1} with valid regex")
     @MethodSource("dataProvider")
-    void testValidityDateAndSymbol(String dateReg, String symbolReg) {
+    void testValidityDateAndSymbolWithDifferentFMP(String dateReg, String fmp) {
         given()
                 .spec(spec)
                 .queryParam("apikey", TOKEN)
                 .when()
-                .get("/AAPL")
+                .get(fmp)
                 .then()
                 .log().all()
                 .body("[0].date", Matchers.matchesPattern(dateReg))
-                .body("[0].symbol", Matchers.matchesPattern(symbolReg));
+                .body("[0].symbol", Matchers.equalTo(fmp));
     }
-
 
     static List<String[]> dataProvider() {
-        String[] str1 = new String[]{"^\\d{4}-\\d{2}-\\d{2}$", "[A-Z]+"};
-        List<String[]> list = new ArrayList<>();
-        list.add(str1);
+        String dateReg = "\\d{4}-\\d{2}-\\d{2}";
+        String[] str1 = new String[]{dateReg, "AAPL"};
+        String[] str2 = new String[]{dateReg, "FB"};
+        String[] str3 = new String[]{dateReg, "GOOG"};
+        String[] str4 = new String[]{dateReg, "MSFT"};
+        String[] str5 = new String[]{dateReg, "NVDA"};
 
-        return list;
+        return List.of(str1, str2, str3, str4, str5);
+    }
+
+    //параметризованный тест на проверку количества объектов
+    @ParameterizedTest(name = "#{index} - test with {0}")
+    @ValueSource(strings = {"AAPL", "FB", "GOOG", "MSFT", "NVDA"})
+    void testGettingDifferentJSONObjectsAndCheckingLength(String arg) {
+        //не смог придумать как по-другому получить объекты JSON, чтобы не дублировать код =(
+        List<EarningsSurprises> list;
+        Response response = given()
+                .spec(spec)
+                .queryParam("apikey", TOKEN)
+                .when()
+                .get(arg);
+        list = response.jsonPath().getList("", EarningsSurprises.class);
+
+        given()
+                .spec(spec)
+                .queryParam("apikey", TOKEN)
+                .when()
+                .get(arg)
+                .then()
+                .log().all()
+                .body("size()", Matchers.equalTo(list.size()));
     }
 }
-
-//получение объектов json
-//        List<String> list;
-//        Response response = given()
-//                .spec(spec)
-//                .queryParam("apikey", TOKEN)
-//                .when()
-//                .get("/AAPL");
-//        list = response.jsonPath().getList("");
