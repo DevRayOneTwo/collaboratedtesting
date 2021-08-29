d1e0d3c8a189747310b5ed3227ea4e813a84959e
Bulat Maskurov <mbmhd2015@gmail.com>
2021-08-29 13:56:36 +0300

    Initial commit: created an empty class EtfList

---
+++ src/test/java/ru/devray/day13/collaboratedtesting/bulkashmak/EtfList.java
@@ -0,0 +1,10 @@
+package ru.devray.day13.collaboratedtesting.bulkashmak;
+
+public class EtfList {
+
+    final static String TOKEN = "883eb2b944916cdc008bb115eefe990b";
+    final static String URI = "https://financialmodelingprep.com/api/v3";
+    final static String PATH = "/etf/list";
+
+
+}
